430c554a27a6395a75c1aab317bce54f91b854d8
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2023-10-30 17:20:18 +0100

    Implemented DiffUtil Optimization for RecyclerView

---
+++ app/src/main/java/com/miguelalvrub/superheroes/features/list/SuperHeroDiffUtil.kt
@@ -0,0 +1,21 @@
+package com.miguelalvrub.superheroes.features.list
+
+import androidx.recyclerview.widget.DiffUtil
+import com.miguelalvrub.superheroes.features.list.domain.GetSuperHeroesFeedUseCase
+
+class SuperHeroDiffUtil : DiffUtil.ItemCallback<GetSuperHeroesFeedUseCase.Output>() {
+
+    override fun areItemsTheSame(
+        oldItem: GetSuperHeroesFeedUseCase.Output,
+        newItem: GetSuperHeroesFeedUseCase.Output
+    ): Boolean {
+        return oldItem.superHero.id == newItem.superHero.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: GetSuperHeroesFeedUseCase.Output,
+        newItem: GetSuperHeroesFeedUseCase.Output
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
